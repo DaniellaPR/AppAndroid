dd00d08fea90d958c440f33fd2ec24c68ec744cc
DaniellaPR <pauladanpz26@gmail.com>
2026-04-05 16:03:26 -0500

    Update: Ajuste ed color del mensaje de bienvenida

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -39,7 +39,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     ) {
         Text(
             text = "Bienvenido al curso 2026, Android, $name",
-            color = Color.Blue,
+            color = Color.Gray,
             style = MaterialTheme.typography.headlineMedium
         )
     }
